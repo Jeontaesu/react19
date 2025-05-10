5d56fb319239000498d27bc31df624618564d926
Jeontaesu <jeontaesu@gmail.com>
2025-05-10 11:38:36 +0900

    style: 코드, 주석 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,6 @@
 import "./app.css";
 
 function App() {
-  const categories = [
-    { name: "Fruites", items: ["Apple", "Cherry", "Watermelon"] },
-    { name: "Vegitable", items: ["Carrot", "Cuccumber", "Lettuce"] },
-  ];
-  return (
-    <>
-      {categories.map((el, idx) => (
-        <div key={idx}>
-          <h1>{el.name}</h1>
-          <ul>
-            {el.items.map((el, idx) => (
-              <li key={idx}>{el}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </>
-  );
+  return <></>;
 }
 export default App;
-
-/*
-  JSX안쪽에서 중첩 반복문 생성 방법
-  - 반복문 안쪽에 또 다른 반복문으로 JSX를 중첩해서 생성하는 방법
-*/
