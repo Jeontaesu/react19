b08b901661271f74a1c0ef41e5060698da11ce19
Jeontaesu <jeontaesu@gmail.com>
2025-05-11 10:05:21 +0900

    style: 코드, 주석 초기화

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,6 @@
 import "./app.css";
-import List from "./components/List";
 
 function App() {
-  const colors = ["red", "green", "blue"];
-  const hobbies = ["game", "reading", "music"];
-
-  return (
-    <>
-      <List arrData={colors} />
-      <List arrData={hobbies} />
-      <List />
-    </>
-  );
+  return <></>;
 }
 export default App;
