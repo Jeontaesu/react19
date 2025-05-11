f9b5c8967645a55d5fe43b07bdbb35fa0216b0ed
Jeontaesu <jeontaesu@gmail.com>
2025-05-11 09:36:51 +0900

    style: 코드, 주석 초기화

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -3,5 +3,6 @@ export default function Btn({ label, handleClick }) {
   // if (!label) label = defaultOpt.label;
   // if (!handleClick) handleClick = defaultOpt.handleClick;
 
+  //삼항 연산자 활용하는 게 더 좋다.
   return <button onClick={handleClick ? handleClick : defaultOpt.handleClick}>{label ? label : defaultOpt.label}</button>;
 }
