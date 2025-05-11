f9939e75edee9b7bd5bf88cee3cd8ceb5cf3efbf
Jeontaesu <jeontaesu@gmail.com>
2025-05-11 10:04:47 +0900

    feat: 비구조화할당으로 props 전달시 default parameter 설정

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,4 +1,5 @@
-export default function List({ arrData }) {
+//비구조화할당으로 props 전달시 아래와 같이 default parameter 설정가능능
+export default function List({ arrData = [1, 2, 3] }) {
   if (!arrData) return console.error("해당 컴포넌트에는 배열데이터가 전달되어야 합니다.");
   return (
     <ul>
