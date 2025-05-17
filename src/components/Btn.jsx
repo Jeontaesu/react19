8eabbbcc6be972f39bbb72d8878bfedda7870377
Jeontaesu <jeontaesu@gmail.com>
2025-05-17 09:26:26 +0900

    feat: 리액트 훅의 기본 개념과 useState사용법

--- src/components/Btn.jsx
+++
@@ -1,8 +0,0 @@
-export default function Btn({ label, handleClick }) {
-  const defaultOpt = { label: "button", handleClick: null };
-  // if (!label) label = defaultOpt.label;
-  // if (!handleClick) handleClick = defaultOpt.handleClick;
-
-  //삼항 연산자 활용하는 게 더 좋다.
-  return <button onClick={handleClick ? handleClick : defaultOpt.handleClick}>{label ? label : defaultOpt.label}</button>;
-}
--- src/components/Card.jsx
+++
@@ -1,8 +0,0 @@
-export default function Card(props) {
-  console.log(props);
-  return (
-    <article>
-      <img src={`https://images.tmdb.org/t/p/w500${props.poster_path}`} />
-    </article>
-  );
-}
